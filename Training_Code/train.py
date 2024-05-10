b2b5b68beda9ef6368cfcef4295d53bf0b475304
JSenY <yeohjunsen@gmail.com>
2024-05-10 17:55:53 +0800

    modified training code

--- Training_Code/train.py
+++ Training_Code/train.py
@@ -4,4 +4,6 @@ from ultralytics import YOLO
 model = YOLO("yolov8m.yaml")  # build a new model from scratch
 
 # Use the model
-model.train(data="config.yaml", epochs=1)  # train the model
+model.train(data="config.yaml", batch = 8, imgsz = 640, epochs = 100, workers = 1)
+
+# CHANGE THE PATH INSIDE OF config.yaml
