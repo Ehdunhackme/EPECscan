43302a985530a2f0e441baa2365c6e5f68673dd2
Dun Li <dunlinmy88@gmail.com>
2024-05-12 01:12:33 +0800

    testing

--- Training_Code/train.py
+++ Training_Code/train.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 model = YOLO("yolov8m.yaml").cuda()  # build a new model from scratch
 
 # Use the model
-model.train(data="config.yaml", batch = 8, imgsz = 640, epochs = 5, workers = 1)
+model.train(data="C:\Users\dunli\Documents\STSY-project-main\config.yaml", batch = 8, imgsz = 640, epochs = 5, workers = 1)
 
 # CHANGE THE PATH INSIDE OF config.yaml
